e06eca90975a54e0f82367904589ab403179f72f
Alex Faissal <alefaissal@gmail.com>
2019-11-25 12:57:26 -0800

    Add Map to App

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import {Text, Image, View} from 'react-native';
-import styles from './styles';
+import {Image} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 const Map = props => {
-  return <Text>Map here</Text>;
+  return (
+    <MapView
+      region={{
+        latitude: 49.2629178,
+        longitude: -123.138583,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      }}
+      zoomEnabled={true}
+      zoomControlEnabled={true}
+      style={{flex: 1}}
+      showsCompass={true}>
+      <Marker coordinate={{latitude: 49.2629178, longitude: -123.138583}}>
+        <Image source={require('../../assets/images/map_pin.png')} />
+      </Marker>
+    </MapView>
+  );
 };
 
 export default Map;
